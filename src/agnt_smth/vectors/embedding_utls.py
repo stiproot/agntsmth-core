eba7d82d6886cc878db2ae32e02fedbacbd6a33e
stiproot <stipcich.simon@gmail.com>
2024-10-26 16:55:27 +0200

    fix: correct ref path for utls in embed fn.

--- src/agnt_smth/vectors/embedding_utls.py
+++ src/agnt_smth/vectors/embedding_utls.py
@@ -1,19 +1,20 @@
-import os
-from typing import List, Dict, Awaitable
-from langchain_core.tools import tool
-from ..utls import log, traverse_folder
+from typing import Awaitable
+from ..core.utls import log, traverse_folder
 
-def embed_file_system_contents(file_system_path: str) -> Awaitable:
+
+async def embed_file_system_contents(file_system_path: str) -> Awaitable:
     """Embeds the contents of a file system into a dictionary.
 
     Args:
       file_system_path: The path to the file system to embed.
 
     Returns:
-      Awaitable: an empty promise. 
+      Awaitable: an empty promise.
     """
 
     log(f"embed_file_system_contents START. file_system_path: {file_system_path}")
 
-    file_tree = traverse_folder(folder_path=file_system_path, ignore_folders=[".git", "__pycache__", "node_modules"])
-
+    file_tree = traverse_folder(
+        folder_path=file_system_path,
+        ignore_folders=[".git", "__pycache__", "node_modules"],
+    )
