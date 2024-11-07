e7bc625b12b2c77811d15e2f289a3ef1b66f861d
stiproot <stipcich.simon@gmail.com>
2024-11-07 08:29:51 +0200

    feat: config path confgiuration.

--- src/agnt_smth/core/utls/config_loader.py
+++ src/agnt_smth/core/utls/config_loader.py
@@ -1,8 +1,17 @@
+from typing import Optional
 from json import load as json_load
 import os
+from .env import EnvVarProvider
 
 
-def load_openai_config(config_file_path: str = ".config/openai_config.json"):
+DEFAULT_CONFIG_FILE_PATH = ".config/openai_config.json"
+env = EnvVarProvider()
+
+
+def load_openai_config(config_file_path: Optional[str] = None):
+    if config_file_path is None:
+        config_file_path = env.get_env_var("CONFIG_PATH", DEFAULT_CONFIG_FILE_PATH)
+
     with open(config_file_path) as f:
         openai_config = json_load(f)
         os.environ["AZURE_OPENAI_API_KEY"] = openai_config["openai_api_key"]
--- src/agnt_smth/vectors/embedding_utls.py
+++ src/agnt_smth/vectors/embedding_utls.py
@@ -3,7 +3,7 @@ from ..core.utls import log, traverse_folder
 
 
 async def embed_file_system_contents(file_system_path: str) -> Awaitable:
-    """Embeds the contents of a file system into a dictionary.
+    """Embeds the contents of a file system.
 
     Args:
       file_system_path: The path to the file system to embed.
@@ -20,3 +20,57 @@ async def embed_file_system_contents(file_system_path: str) -> Awaitable:
     )
 
     log(f"file_tree: {file_tree}")
+
+
+async def embed_repo(repo_name: str, repo_target_dir: str) -> Awaitable:
+    log(f"{embed_repo.__name__} START.")
+
+    ignore_folders = ["node_modules", ".git", "bin", "obj", "__pycache__"]
+    ignore_extensions = [
+        ".pfx",
+        ".crt",
+        ".pem",
+        ".postman_collection.json",
+        ".postman_environment",
+        ".png",
+        ".gif",
+        ".jpeg",
+        ".jpg",
+        ".ico",
+        ".svg",
+        ".woff",
+        ".woff2",
+        ".ttf",
+        ".gz",
+        ".zip",
+        ".tar",
+        ".tgz",
+        ".tar.gz",
+        ".rar",
+        ".7z",
+        ".pdf",
+        ".doc",
+        ".docx",
+        ".xls",
+        ".xlsx",
+        ".ppt",
+        ".pptx"
+    ]
+
+    file_dict = traverse_folder(repo_target_dir, ignore_folders, ignore_extensions)
+    file_paths = [f"{k}/{f}" for k, v in file_dict.items() for f in v]
+    log(f"{embed_repo.__name__} -> file_paths: {file_paths}")
+
+    chroma_client = ChromaHttpClientFactory.create_with_auth()
+    embedding_function = EmbeddingFactory.create()
+
+    chunk_embed_and_publish(
+        file_paths=file_paths,
+        collection_name=repo_name,
+        embedding_function=embedding_function,
+        chroma_client=chroma_client,
+        chunk_size=4000,
+        chunk_overlap=200,
+    )
+
+    log(f"{embed_repo.__name__} END.")
