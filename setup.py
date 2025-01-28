dc707bf7a05b7365f01b8111fe90d3d6716c05f5
stiproot <stipcich.simon@gmail.com>
2025-01-28 19:32:39 +0200

    fix: remove unused langchain community ref.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ author_email = "stipcich.simon@gmail.com"
 url = "https://github.com/stiproot/agntsmth-core"
 license = "MIT"
 keywords = ["python", "package", "langchain", "langgraph", "agentic", "beta"]
-version = "0.1.1"
+version = "0.1.2"
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
